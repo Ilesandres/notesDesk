95f480c1ab149f62ceedd8a26eeefa79e92ce1b4
Ilesandres <ilesandres6@gmail.com>
2025-10-20 10:12:20 -0500

    sidebar y contentBar deisposicion de espacio

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,12 @@
+import { Content, RootLayout, Sidebar } from "@/components"
 
 function App(){
 
   return (
-    <>
-
-    </>
+<RootLayout>
+  <Sidebar className="p-2 border-4 border-red-500">sidebar</Sidebar>
+  <Content className="border-4 border-blue-500"> Content</Content>
+</RootLayout>
   )
 }
 
