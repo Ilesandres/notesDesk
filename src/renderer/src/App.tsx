92e0b6674230ddb19f8222ae16587c4553109bf6
Ilesandres <ilesandres6@gmail.com>
2025-10-21 09:48:34 -0500

    floatingTitle implementation

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import {  ActionButtonsRows,Content, DraggableTopBar, MarkdownEditor, NotePreviewList, RootLayout, Sidebar, } from "@/components"
+import { ActionButtonsRows, Content, DraggableTopBar, FloatingNoteTitle, MarkdownEditor, NotePreviewList, RootLayout, Sidebar, } from "@/components"
 
 const App = () => {
 
@@ -7,12 +7,13 @@ const App = () => {
       <DraggableTopBar />
       <RootLayout>
         <Sidebar className="p-2">
-          <ActionButtonsRows className="flex justify-between mt-1"/>
-          <NotePreviewList className="mt-3 space-y-1"/>
+          <ActionButtonsRows className="flex justify-between mt-1" />
+          <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
         <Content className="border- bg-zinc-900/50 border-l-white/20">
-         <MarkdownEditor/>
-         </Content>
+        <FloatingNoteTitle className="pt-2"/>
+          <MarkdownEditor />
+        </Content>
       </RootLayout>
     </>
   )
